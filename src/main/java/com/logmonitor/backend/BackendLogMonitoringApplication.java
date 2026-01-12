47295b8541fb683521bdad117c6741f7734898eb
Mohd Aasif <aasifmohd1385@gmail.com>
2026-01-12 15:08:22 +0530

    this is my first commit

---
+++ src/main/java/com/logmonitor/backend/BackendLogMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.logmonitor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLogMonitoringApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(BackendLogMonitoringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/logmonitor/backend/BackendLogMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmonitor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLogMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
